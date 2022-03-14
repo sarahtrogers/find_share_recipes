36fe786e3a0a61b12463735c344f9b912b9be6ed
anonymous anonymous <sarah.rogers@kellogg.northwestern.edu>
2022-03-14 01:50:02 +0000

    Whitelist columns in Recipe for ActiveAdmin

--- app/admin/recipes.rb
+++ app/admin/recipes.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Recipe do
 
+ permit_params :photo, :name, :ingredients, :category_id, :dietary_restrictions, :user_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
