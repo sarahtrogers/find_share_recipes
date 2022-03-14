a1b52f740d692f34a3ae1bdb51622b14fe22931b
anonymous anonymous <sarah.rogers@kellogg.northwestern.edu>
2022-03-14 01:49:53 +0000

    Validate presence of recipes.photo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,8 @@ class Recipe < ApplicationRecord
 
   # Validations
 
+  validates :photo, :presence => true
+
   validates :user_id, :presence => true
 
   # Scopes
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Recipe, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:photo) }
+
     it { should validate_presence_of(:user_id) }
 
     end
