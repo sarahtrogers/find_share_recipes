c4d80b8b81312abe28890ce1c7b01d6662d04fc4
anonymous anonymous <sarah.rogers@kellogg.northwestern.edu>
2022-03-14 01:50:10 +0000

    Whitelist columns in Share for ActiveAdmin

--- app/admin/shares.rb
+++ app/admin/shares.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Share do
 
+ permit_params :recipe_id, :recipient_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
