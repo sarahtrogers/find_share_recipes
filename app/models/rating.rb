56e1bc267da6c57bbf46eea71828738a2730f0a8
anonymous anonymous <sarah.rogers@kellogg.northwestern.edu>
2022-03-14 01:49:45 +0000

    Generate ratings resource with ideas:scaffold

---
+++ app/models/rating.rb
@@ -0,0 +1,14 @@
+class Rating < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    rating
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ratings
   resources :recipes
   devise_for :users
   resources :users
