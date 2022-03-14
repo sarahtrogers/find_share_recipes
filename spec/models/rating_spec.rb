b27751df6b5f21d2e97e546fa9b9224b7b554cbf
anonymous anonymous <sarah.rogers@kellogg.northwestern.edu>
2022-03-14 01:49:53 +0000

    Validate presence of ratings.rating

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Rating, type: :model do
 
     describe "Validations" do
 
+    it { should validate_presence_of(:rating) }
+
     it { should validate_presence_of(:recipe_id) }
 
     it { should validate_presence_of(:user_id) }
